975c335f8e61826ddfdb0cffe0f170528c7ccfc2
QuickRecon <aren.leishman@gmail.com>
2017-09-09 20:37:59 +0800

    Add table values Add constructor to Deco class

--- Deco.cpp
+++ Deco.cpp
@@ -3,3 +3,61 @@
 //
 
 #include "Deco.h"
+
+/// Compartment Tables (Taken from Subsurface Deco.c)
+const double Deco::buehlmann_N2_a[] = { 1.1696, 1.0, 0.8618, 0.7562,
+                                  0.62, 0.5043, 0.441, 0.4,
+                                  0.375, 0.35, 0.3295, 0.3065,
+                                  0.2835, 0.261, 0.248, 0.2327 };
+
+const double Deco::buehlmann_N2_b[] = { 0.5578, 0.6514, 0.7222, 0.7825,
+                                  0.8126, 0.8434, 0.8693, 0.8910,
+                                  0.9092, 0.9222, 0.9319, 0.9403,
+                                  0.9477, 0.9544, 0.9602, 0.9653 };
+
+const double Deco::buehlmann_N2_t_halflife[] = { 5.0, 8.0, 12.5, 18.5,
+                                           27.0, 38.3, 54.3, 77.0,
+                                           109.0, 146.0, 187.0, 239.0,
+                                           305.0, 390.0, 498.0, 635.0 };
+
+const double Deco::buehlmann_He_a[] = { 1.6189, 1.383, 1.1919, 1.0458,
+                                  0.922, 0.8205, 0.7305, 0.6502,
+                                  0.595, 0.5545, 0.5333, 0.5189,
+                                  0.5181, 0.5176, 0.5172, 0.5119 };
+
+const double Deco::buehlmann_He_b[] = { 0.4770, 0.5747, 0.6527, 0.7223,
+                                  0.7582, 0.7957, 0.8279, 0.8553,
+                                  0.8757, 0.8903, 0.8997, 0.9073,
+                                  0.9122, 0.9171, 0.9217, 0.9267 };
+
+const double Deco::buehlmann_He_t_halflife[] = { 1.88, 3.02, 4.72, 6.99,
+                                           10.21, 14.48, 20.53, 29.11,
+                                           41.20, 55.19, 70.69, 90.34,
+                                           115.29, 147.42, 188.24, 240.03 };
+
+Deco::gas::gas(double FrN2, double FrO2, double FrHe) {
+    this->FrN2 = FrN2;
+    this->FrO2 = FrO2;
+    this->FrHe = FrHe;
+}
+
+Deco::Deco(double ppWv, double pA) {
+    this->ppWv = ppWv;
+    this->pA = pA;
+
+    /// Configure default gas (Air)
+    gases.push_back(Deco::gas(0.79, 0.21, 0));
+
+    this->ppN2 = gases.data()[0].FrN2 * (this->pA - this->ppWv);
+
+    /// Create gas compartments
+    for(int i=0; i < 16; i++){
+        this->SetGasLoadings(this->ppN2, 0, i);
+    }
+}
+
+void Deco::SetGasLoadings(double Pn, double Ph, int compartmentIndex) {
+    this->Pn[compartmentIndex] = Pn;
+    this->Ph[compartmentIndex] = Ph;
+    this->Pt[compartmentIndex] = Pn + Ph;
+}
--- Deco.h
+++ Deco.h
@@ -5,22 +5,45 @@
 #ifndef DECOCLI_DECO_H
 #define DECOCLI_DECO_H
 
+#include <vector>
 
 class Deco {
-    /// gas parameters
-    double ppN2;        // partial pressure Nitrogen
-    double ppHe;        // parital pressure Helium
-    double ppO2;        // partial pressure Oxygen
+    /// Functions
+    Deco(double ppWv, double pA);
+    void SetGasLoadings(double Pn, double Ph, int compartmentIndex);
+
+    /// Gas storage
     struct gas {
         double FrN2;    // Fractional percent Nitrogen
         double FrO2;    // Fractional percent Oxygen
         double FrHe;    // Fractional percent Helium
+        gas(double FrN2, double FrO2, double FrHe);
     };
+    std::vector<gas> gases;
 
-    /// Environmental Parameters
-    double pA;          // Ambient pressure
-    double ppWv;        // Partial pressure water vapor
+    private:
+        /// gas parameters
+        double ppN2;        // partial pressure Nitrogen
+        double ppHe;        // parital pressure Helium
+        double ppO2;        // partial pressure Oxygen
 
+        /// Gas Loadings
+        double Pn[16];          // Nitrogen gas loading
+        double Ph[16];          // Helium gas loading
+        double Pt[16];          // Total gas loading
+
+        /// Environmental Parameters
+        double pA;          // Ambient pressure
+        double ppWv;        // Partial pressure water vapor
+
+        /// Compartment Tables (Taken from Subsurface Deco.c)
+        static const double buehlmann_N2_a[];
+        static const double buehlmann_N2_b[];
+        static const double buehlmann_N2_t_halflife[];
+
+        static const double buehlmann_He_a[];
+        static const double buehlmann_He_b[];
+        static const double buehlmann_He_t_halflife[];
 };
 
 
