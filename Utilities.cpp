9e0f339c7dcc6fb22a8803a3bb4bcf765c95257b
QuickRecon <aren.leishman@gmail.com>
2017-09-16 20:17:42 +0800

    Adjust spacing

--- Utilities.cpp
+++ Utilities.cpp
@@ -36,32 +36,31 @@ std::vector<float> StringSplit(std::string str, char splitchar){
 
 void ShowLicense(){
     std::string License =
-            "    Program for calculating decompression stops and no stop times using buehlmann and buehlmann derived algorithms\n\n"
-            "    Copyright (C) 2017 Aren Leishman\n"
+            "Program for calculating decompression stops and no stop times using buehlmann and buehlmann derived algorithms\n\n"
+            "Copyright (C) 2017 Aren Leishman\n"
             "\n"
-            "    This program is free software; you can redistribute it and/or modify\n"
-            "    it under the terms of the GNU General Public License as published by\n"
-            "    the Free Software Foundation; either version 2 of the License, or\n"
-            "    (at your option) any later version.\n"
+            "This program is free software; you can redistribute it and/or modify\n"
+            "it under the terms of the GNU General Public License as published by\n"
+            "the Free Software Foundation; either version 2 of the License, or\n"
+            "(at your option) any later version.\n"
             "\n"
-            "    This program is distributed in the hope that it will be useful,\n"
-            "    but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-            "    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-            "    GNU General Public License for more details.\n"
+            "This program is distributed in the hope that it will be useful,\n"
+            "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+            "GNU General Public License for more details.\n"
             "\n"
-            "    You should have received a copy of the GNU General Public License along\n"
-            "    with this program; if not, write to the Free Software Foundation, Inc.,\n"
-            "    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.";
+            "You should have received a copy of the GNU General Public License\n"
+            "along with this program.  If not, see <http://www.gnu.org/licenses/>.";
 
     std::cout << License << std::endl << std::endl;
 }
 
 void ShowLimitedLicense(){
     std::string License =
-            "    DecoCLI  Copyright (C) 2017 Aren Leishman\n"
-            "    This program comes with ABSOLUTELY NO WARRANTY.\n"
-            "    This is free software, and you are welcome to redistribute it\n"
-            "    under certain conditions; use the ShowLicense option for details";
+            "DecoCLI  Copyright (C) 2017 Aren Leishman\n"
+            "This program comes with ABSOLUTELY NO WARRANTY.\n"
+            "This is free software, and you are welcome to redistribute it\n"
+            "under certain conditions; use the ShowLicense option for details";
     std::cout << License << std::endl << std::endl;
 }
 
--- main.cpp
+++ main.cpp
@@ -24,30 +24,30 @@
 bool AutoShowLicense = true;
 bool verbose = true;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
     GFDeco DecoActual; //initialise Deco Model
 
-    for(int i = 0; i < argc; i++){
+    for (int i = 0; i < argc; i++) {
         std::string argument(argv[i]);
-        std::string FirstLetter = argument.substr(0,1);
-        if(argument == "--ShowLicense"){
+        std::string FirstLetter = argument.substr(0, 1);
+        if (argument == "--ShowLicense") {
             ShowLicense();
             return 0;
-        } else if(argument ==  "--HideLicense"){
+        } else if (argument == "--HideLicense") {
             AutoShowLicense = false;
-        } else if( FirstLetter == "G"){
-            std::string substring = argument.substr(1,argument.size());
+        } else if (FirstLetter == "G") {
+            std::string substring = argument.substr(1, argument.size());
             std::vector<float> parameters = StringSplit(substring, ':');
             DecoActual.AddGas(parameters[0], parameters[1], parameters[3]);
-        } else if(argument == "--quite") {
+        } else if (argument == "--quite") {
             verbose = false;
             AutoShowLicense = false;
             return 0;
         }
     }
-    if(AutoShowLicense){ShowLimitedLicense();};
-    if(argc >= 3){
+    if (AutoShowLicense) { ShowLimitedLicense(); };
+    if (argc >= 3) {
 
         //depths are in bar, times in min
         DecoActual.AddDecent(MeterToBar(60), MeterToBar(DecoActual.DecentRate));
